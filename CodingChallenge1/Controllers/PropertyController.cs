a3744e5002b21b9ed95894668f8320abd07f4671
Kannan KR <kannan.k@kiwitech.com>
2018-05-13 12:11:56 +0530

    Add Basic Property Details Page

---
+++ CodingChallenge1/Controllers/PropertyController.cs
@@ -0,0 +1,26 @@
+﻿using CodingChallenge1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodingChallenge1.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public PropertyController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [Route("Property/{propertyId}")]
+        public ActionResult Index(Guid propertyId)
+        {
+            var property = _dbContext.Properties.Find(propertyId);
+            return View(property);
+        }
+    }
+}
